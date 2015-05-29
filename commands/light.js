a6489648f298fc4c48a338ccbf181d9462c83728
Josh Cole <saikojosh@gmail.com>
2015-05-29 21:06:44 +0100

    Added placeholder modules for OS and light commands

---
+++ commands/light.js
@@ -0,0 +1,40 @@
+/*
+ * LIGHT COMMANDS
+ */
+
+var ME = module.exports;
+
+/*
+ * Set the light to a specific level.
+ */
+ME['set'] = function (req, res) {
+
+};
+
+/*
+ * Turn the light fully off.
+ */
+ME['off'] = function (req, res) {
+
+};
+
+/*
+ * Turn the light fully on.
+ */
+ME['on'] = function (req, res) {
+
+};
+
+/*
+ * Gradually increase the light level over a set period of time.
+ */
+ME['wake-up'] = function (req, res) {
+
+};
+
+/*
+ * Gradually decrease the light level over a set period of time.
+ */
+ME['sleep'] = function (req, res) {
+
+};
---
+++ commands/os.js
@@ -0,0 +1,19 @@
+/*
+ * OS COMMANDS
+ */
+
+var ME = module.exports;
+
+/*
+ * Reboot the system.
+ */
+ME['reboot'] = function (req, res) {
+
+};
+
+/*
+ * Shut down the system.
+ */
+ME['shut-down'] = function (req, res) {
+
+};
