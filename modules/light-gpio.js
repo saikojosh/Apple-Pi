01e16bc73ea9aa3e206e4417826142265e463405
Josh Cole <saikojosh@gmail.com>
2015-05-30 19:10:22 +0100

    Added pin definitions to light GPIO module

---
+++ modules/light-gpio.js
@@ -0,0 +1,20 @@
+/*
+ * LIGHT GPIO
+ */
+
+var GPIO = require('onoff').Gpio;
+
+/*
+ * Export the GPIO for the light.
+ * The light output pins are: 0, 1, 4, 7, 8, 9, 10 & 11.
+ */
+module.exports = [
+  new GPIO(0,  'out'),
+  new GPIO(1,  'out'),
+  new GPIO(4,  'out'),
+  new GPIO(7,  'out'),
+  new GPIO(8,  'out'),
+  new GPIO(9,  'out'),
+  new GPIO(10, 'out'),
+  new GPIO(11, 'out')
+];
