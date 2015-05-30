9f5ec96dec5edf6bf92786b540db254f388126e1
Josh Cole <saikojosh@gmail.com>
2015-05-30 14:42:45 +0100

    Implemented OS commands reboot/shutdown

--- commands/os.js
+++ commands/os.js
@@ -2,18 +2,29 @@
  * OS COMMANDS
  */
 
-var ME = module.exports;
+var ME    = module.exports;
+var shell = require('../modules/shell');
 
 /*
  * Reboot the system.
  */
 ME['reboot'] = function (req, res) {
 
+  shell.exec('shutdown', '-r -i6 -g15 "*** Rebooting Now! ****"', function (err) {
+    if (err) { return res.errorOut(500, 'Command failed.'); }
+    return res.dataOut(true);
+  });
+
 };
 
 /*
  * Shut down the system.
  */
-ME['shut-down'] = function (req, res) {
+ME['shutdown'] = function (req, res) {
+
+  shell.exec('shutdown', '-y -i6 -g15 "*** Shutting Down Now! ****"', function (err) {
+    if (err) { return res.errorOut(500, 'Command failed.'); }
+    return res.dataOut(true);
+  });
 
 };
