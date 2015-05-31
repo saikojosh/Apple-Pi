aa3ed44f0cc35393c54e6edee9ec0c9b9f545d4a
Josh Cole <saikojosh@gmail.com>
2015-05-31 16:48:26 +0100

    Added light comm module console messages

--- commands/light.comm.js
+++ commands/light.comm.js
@@ -2,14 +2,17 @@
  * LIGHT COMMANDS
  */
 
-var ME    = module.exports;
-var async = require('async');
-var light = require('../modules/light');
+var ME     = module.exports;
+var async  = require('async');
+var logger = require('log-ninja');
+var light  = require('../modules/light');
 
 /*
  * Set the light to a specific level.
  */
 ME['set'] = function (req, res) {
+
+  logger.info('[light.comm] SET ' + req.body.value);
 
   // Change the light value.
   light.set(req.body.value, function (err) {
@@ -24,6 +27,8 @@ ME['set'] = function (req, res) {
  */
 ME['off'] = function (req, res) {
 
+  logger.info('[light.comm] OFF');
+
   // Turn off the light completely.
   light.set(0, function (err) {
     if (err) { return res.errorOut(500, 'Failed to set light value.'); }
@@ -36,6 +41,8 @@ ME['off'] = function (req, res) {
  * Turn the light fully on.
  */
 ME['on'] = function (req, res) {
+
+  logger.info('[light.comm] ON');
 
   // Turn on the light completely.
   light.set(255, function (err) {
@@ -50,6 +57,8 @@ ME['on'] = function (req, res) {
  */
 ME['wake-up'] = function (req, res) {
 
+  logger.info('[light.comm] WAKE UP ' + req.body.duration);
+
   light.graduate(0, 255, req.body.duration, function (err) {
     if (err) { return res.errorOut(500, 'Failed to set light value.'); }
     return res.dataOut(true);
@@ -61,6 +70,8 @@ ME['wake-up'] = function (req, res) {
  * Gradually decrease the light level over a set period of time.
  */
 ME['sleep'] = function (req, res) {
+
+  logger.info('[light.comm] SLEEP ' + req.body.duration);
 
   light.graduate(255, 0, req.body.duration, function (err) {
     if (err) { return res.errorOut(500, 'Failed to set light value.'); }
