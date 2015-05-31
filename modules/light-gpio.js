14561a66ac75d0c405b9ed018760292bf1c3bda8
Josh Cole <saikojosh@gmail.com>
2015-05-31 17:41:31 +0100

    Fix - pins in reverse order

--- modules/light-gpio.js
+++ modules/light-gpio.js
@@ -8,12 +8,12 @@ var GPIO = require('onoff').Gpio;
  * Export the GPIO for the light.
  */
 module.exports = [
-  new GPIO(2,  'out'),
-  new GPIO(3,  'out'),
-  new GPIO(4,  'out'),
-  new GPIO(7,  'out'),
-  new GPIO(8,  'out'),
-  new GPIO(9,  'out'),
+  new GPIO(11, 'out'),
   new GPIO(10, 'out'),
-  new GPIO(11, 'out')
+  new GPIO(9,  'out'),
+  new GPIO(8,  'out'),
+  new GPIO(7,  'out'),
+  new GPIO(4,  'out'),
+  new GPIO(3,  'out'),
+  new GPIO(2,  'out')
 ];
